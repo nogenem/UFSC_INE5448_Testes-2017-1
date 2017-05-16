0583d03b3f0472d1ab573f4ff00cbe4f189c6f1a
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-05-16 10:39:24 -0300

    Adição do projeto base pro exercício 7

---
+++ GerenciadorDeOcorrencias_ex7/src/testes/TesteExemplo.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteExemplo {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
