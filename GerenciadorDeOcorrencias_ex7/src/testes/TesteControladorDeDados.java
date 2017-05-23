152f8834511a81441d0a268b39cb2c5f03c8d87e
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-05-23 13:29:13 -0300

    Adição de 3 novos testes para verificar o ...
    cadastro de mais de um funcionário/projeto/ocorrência

--- GerenciadorDeOcorrencias_ex7/src/testes/TesteControladorDeDados.java
+++ GerenciadorDeOcorrencias_ex7/src/testes/TesteControladorDeDados.java
@@ -1,8 +1,6 @@
 package testes;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -33,6 +31,16 @@ public class TesteControladorDeDados {
 		assertTrue(controlador.funcionarioEstaCadastrado(uidFunc));
 	}
 	
+	@Test
+	public void cadastraDoisNovosFuncionarios() throws Exception {
+		long uidFunc1 = this.cadastraFuncionarioValido();
+		long uidFunc2 = this.cadastraFuncionarioValido();
+		
+		assertEquals(2, controlador.getNumeroDeFuncionarios());
+		assertTrue(controlador.funcionarioEstaCadastrado(uidFunc1));
+		assertTrue(controlador.funcionarioEstaCadastrado(uidFunc2));
+	}
+	
 	// Cadastro de projeto
 	@Test
 	public void cadastraUmNovoProjeto() throws Exception {
@@ -43,6 +51,16 @@ public class TesteControladorDeDados {
 		assertTrue(controlador.projetoEstaCadastrado(uidProj));
 	}
 	
+	@Test
+	public void cadastraDoisNovosProjetos() throws Exception {
+		long uidProj1 = this.cadastraProjetoValido();
+		long uidProj2 = this.cadastraProjetoValido();
+		
+		assertEquals(2, controlador.getNumeroDeProjetos());
+		assertTrue(controlador.projetoEstaCadastrado(uidProj1));
+		assertTrue(controlador.projetoEstaCadastrado(uidProj2));
+	}
+	
 	// Cadastro de ocorrencia
 	@Test
 	public void cadastraUmaNovaOcorrencia() throws Exception {
@@ -51,6 +69,16 @@ public class TesteControladorDeDados {
 		assertEquals(1, uidOcorrencia);
 		assertEquals(1, controlador.getNumeroDeOcorrenciasAbertas());
 		assertTrue(controlador.ocorrenciaEstaAberta(uidOcorrencia));
+	}
+	
+	@Test
+	public void cadastraDuasNovasOcorrencias() throws Exception {
+		long uidOcorrencia1 = this.cadastraOcorrenciaValida();
+		long uidOcorrencia2 = this.cadastraOcorrenciaValida();
+		
+		assertEquals(2, controlador.getNumeroDeOcorrenciasAbertas());
+		assertTrue(controlador.ocorrenciaEstaAberta(uidOcorrencia1));
+		assertTrue(controlador.ocorrenciaEstaAberta(uidOcorrencia2));
 	}
 	
 	@Test(expected=Exception.class)
