128cecdc00de85c7f736e59987e95784fa4262e7
gilney nathanael mathias <gilney_salvo@hotmail.com>
2017-05-24 16:20:28 -0300

    Adicionado o atributo participantes a classe ...
    Projeto e os devidos métodos e testes necessários para sua manipulação

--- GerenciadorDeOcorrencias_ex7/src/main/model/Projeto.java
+++ GerenciadorDeOcorrencias_ex7/src/main/model/Projeto.java
@@ -10,11 +10,13 @@ public class Projeto {
 	private long uid;
 	private String nome;
 	private Set<Ocorrencia> ocorrencias;
+	private Set<Funcionario> participantes;
 	
 	public Projeto(String nome){
 		this.uid = ++CURRENT_UID;
 		this.nome = nome;
 		this.ocorrencias = new HashSet<>();
+		this.participantes = new HashSet<>();
 	}
 
 	public long getUid() {
@@ -29,8 +31,39 @@ public class Projeto {
 		return ocorrencias;
 	}
 	
-	public boolean addOcorrencia(Ocorrencia ocorrencia){
-		return this.ocorrencias.add(ocorrencia);
+	public void addOcorrencia(Ocorrencia ocorrencia) throws Exception {
+		boolean retorno = this.getOcorrencias().add(ocorrencia);
+		if(!retorno)
+			throw new Exception("Ocorrencia com uid = " +ocorrencia.getUid()+ 
+					" ja faz parte do projeto com uid = " +this.uid+ "!");
+		
+		//Funcionários responsáveis por ocorrências pertencentes a um projeto
+		//devem ser participantes deste projeto
+		Funcionario responsavel = ocorrencia.getResponsavel();
+		if(responsavel == null)
+			throw new Exception("Tentativa de adicionar uma ocorrencia com uid = " +ocorrencia.getUid()+ 
+					" ao projeto com uid = " +this.uid+ " sem que a ocorrencia possua um responsavel!");
+		if(!this.getParticipantes().contains(responsavel))
+			this.addParticipante(responsavel);
+	}
+	
+	public int getNumeroDeOcorrencias() {
+		return this.getOcorrencias().size();
+	}
+	
+	public Set<Funcionario> getParticipantes(){
+		return this.participantes;
+	}
+	
+	public void addParticipante(Funcionario participante) throws Exception {
+		boolean retorno = this.getParticipantes().add(participante);
+		if(!retorno)
+			throw new Exception("Funcionario com uid = " +participante.getUid()+ 
+					" ja participa do projeto com uid = " +this.uid+ "!");
+	}
+	
+	public int getNumeroDeParticipantes(){
+		return this.getParticipantes().size();
 	}
 	
 	public static void zerarUID(){
--- GerenciadorDeOcorrencias_ex7/src/testes/TesteProjeto.java
+++ GerenciadorDeOcorrencias_ex7/src/testes/TesteProjeto.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertNotEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import main.model.Funcionario;
+import main.model.Ocorrencia;
 import main.model.Projeto;
 
 public class TesteProjeto {
@@ -22,6 +24,90 @@ public class TesteProjeto {
 		assertEquals(1, projetoTeste.getUid());
 		assertEquals("Projeto teste", projetoTeste.getNome());
 		assertEquals(0, projetoTeste.getOcorrencias().size());
+	}
+	
+	@Test
+	public void adicionaUmaOcorrenciaAoProjetoComResponsavelNaoPertencenteAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Ocorrencia ocorrencia = this.criaNovaOcorrenciaValida();
+		
+		projetoTeste.addOcorrencia(ocorrencia);
+		
+		assertEquals(1, projetoTeste.getNumeroDeOcorrencias());
+	}
+	
+	@Test
+	public void adicionaUmaOcorrenciaAoProjetoComResponsavelPertencenteAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Ocorrencia ocorrencia = this.criaNovaOcorrenciaValida();
+		
+		projetoTeste.addParticipante(ocorrencia.getResponsavel());
+		projetoTeste.addOcorrencia(ocorrencia);
+		
+		assertEquals(1, projetoTeste.getNumeroDeOcorrencias());
+	}
+	
+	@Test(expected=Exception.class)
+	public void adicionaUmaOcorrenciaAoProjetoSemResponsavel() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Ocorrencia ocorrencia = this.criaNovaOcorrenciaValida();
+		ocorrencia.setResponsavel(null);
+		
+		projetoTeste.addOcorrencia(ocorrencia);
+		
+		assertEquals(1, projetoTeste.getNumeroDeOcorrencias());
+	}
+	
+	@Test
+	public void adicionaDuasOcorrenciasAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Ocorrencia ocorrencia1 = this.criaNovaOcorrenciaValida();
+		Ocorrencia ocorrencia2 = this.criaNovaOcorrenciaValida();
+		
+		projetoTeste.addOcorrencia(ocorrencia1);
+		projetoTeste.addOcorrencia(ocorrencia2);
+		
+		assertEquals(2, projetoTeste.getNumeroDeOcorrencias());
+	}
+	
+	@Test(expected=Exception.class)
+	public void adicionaMesmaOcorrenciaAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Ocorrencia ocorrencia = this.criaNovaOcorrenciaValida();
+		
+		projetoTeste.addOcorrencia(ocorrencia);
+		projetoTeste.addOcorrencia(ocorrencia);
+	}
+	
+	@Test
+	public void adicionaUmParticipanteAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Funcionario funcionario = this.criaNovoFuncionarioValido();
+		
+		projetoTeste.addParticipante(funcionario);
+		
+		assertEquals(1, projetoTeste.getNumeroDeParticipantes());
+	}
+	
+	@Test
+	public void adicionaDoisParticipantesAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Funcionario funcionario1 = this.criaNovoFuncionarioValido();
+		Funcionario funcionario2 = this.criaNovoFuncionarioValido();
+		
+		projetoTeste.addParticipante(funcionario1);
+		projetoTeste.addParticipante(funcionario2);
+		
+		assertEquals(2, projetoTeste.getNumeroDeParticipantes());
+	}
+	
+	@Test(expected=Exception.class)
+	public void adicionaMesmoParticipanteAoProjeto() throws Exception {
+		Projeto projetoTeste = new Projeto("Projeto teste");
+		Funcionario funcionario = this.criaNovoFuncionarioValido();
+		
+		projetoTeste.addParticipante(funcionario);
+		projetoTeste.addParticipante(funcionario);
 	}
 	
 	@Test
@@ -45,5 +131,19 @@ public class TesteProjeto {
 		
 		assertNotEquals(projetoTeste, null);
 	}
+	
+	//Métodos uteis
+	public Ocorrencia criaNovaOcorrenciaValida() throws Exception {
+		Ocorrencia ocorrencia = new Ocorrencia("Bug na classe Avião.", 
+				Ocorrencia.Prioridade.ALTA, Ocorrencia.Tipo.BUG);
+		Funcionario responsavel = this.criaNovoFuncionarioValido();
 
+		ocorrencia.setResponsavel(responsavel);
+		return ocorrencia;
+	}
+	
+	public Funcionario criaNovoFuncionarioValido(){
+		return new Funcionario("Fulano da Silva");
+	}
+	
 }
